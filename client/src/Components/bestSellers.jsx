685cd92aee8cfbf85daa258501b5209f809168a1
Fuadmikayil <mikayilov2006@gmail.com>
2025-03-17 23:16:22 +0400

    BestSellers end

--- client/src/Components/bestSellers.jsx
+++ client/src/Components/bestSellers.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from "react";
+import { Bestproducts } from "../data";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+
+import "../style/custom-swiper.css"; 
 
 const BestSellers = () => {
   return (
-    <>
-    <h1 className='ml-2 text-2xl  tracking-wide font-semibold'>Best Sellers</h1>
-    </>
-  )
-}
+    <div className="bg-[#F6F5F3] py-6 px-2 md:py-10">
+      <h1 className="ml-2  text-2xl md:text-5xl pb-2 tracking-wide font-semibold">Best Sellers</h1>
 
-export default BestSellers
+      <div className="w-full px-4 relative ">
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={20}
+          breakpoints={{
+            640: { slidesPerView: 2 },
+            1024: { slidesPerView: 3 },
+          }}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+          loop={true}
+          navigation={true} 
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          {Bestproducts.map((product, index) => (
+            <SwiperSlide key={index} className="flex flex-col items-center pt-4">
+              <img
+                className="w-[180px] md:w-[300px] mx-auto h-[300px] object-cover rounded-lg hover:-translate-y-4 delay-150 duration-500 "
+                src={product.img}
+                alt={product.name}
+              />
+              <p className="text-center text-xl md:text-[18px] font-bold mt-2">{product.name}</p>
+              <p className="text-center text-[12px] md:text-[14px] font-semibold">{product.text}</p>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+};
+
+export default BestSellers;
--- client/src/data/index.js
+++ client/src/data/index.js
@@ -9,6 +9,13 @@ import ProductMImg5 from "../assets/issey-miyake-a-drop-d-issey-hp-banner-m.jpg"
 import ProductImg6 from "../assets/HP-FDI-1_left_background.jpg";
 import ProductMImg6 from "../assets/HP-FDI-1_center_background.jpg";
 
+import P1Img from "../assets/p1.png";
+import P2Img from "../assets/p2.png";
+import P3Img from "../assets/p3.png";
+import P4Img from "../assets/p4.png";
+import P5Img from "../assets/p5.png";
+import P6Img from "../assets/p6.png";
+
 export const products = [
     {
         name:"LE SEL D'ISSEY THE NEW REFILLABLE FRAGRANCE FOR MEN",
@@ -36,3 +43,40 @@ export const products = [
         mimg: ProductMImg6,
     }
 ];
+export const Bestproducts = [
+    {
+        name:"LE SEL D'ISSEY",
+        img: P1Img,
+        text:"EAU DE TOILETTE"
+    },
+    {
+        name:"L'EAU D'ISSEY",
+        img: P2Img,
+        text:"EAU DE TOILETTE"
+
+    },
+    {
+        name:"L'EAU D'ISSEY POUR HOMME",
+        img: P3Img,
+        text:"EAU DE TOILETTE"
+
+    },
+    {
+        name:"L'EAU D'ISSEY PIVOINE",
+        img: P4Img,
+        text:"EAU DE TOILETTE INTENSE"
+
+    },
+    {
+        name:"L'EAU D'ISSEY POUR HOMME WOOD&WOOD",
+        img: P5Img,
+        text:"EAU DE TOILETTE INTENSE"
+
+    },
+    {
+        name:"Fusion d'Issey ",
+        img: P6Img,
+        text:"EAU DE TOILETTE INTENSE"
+
+    }
+]
